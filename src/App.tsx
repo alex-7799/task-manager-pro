e51340bb569a37402653b8130166ea71c5ccae3c
Thinh-Alex-Le <tql79@outlook.com>
2025-05-29 23:49:48 +1000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Deploy #1
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
